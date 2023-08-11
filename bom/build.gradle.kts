ab4e94b86b860cc81338b21f9f32c029430f902a
millylee-dev <millylee.dev@gmail.com>
2023-08-11 15:28:03 -0700

    Downgrade version to 6.3.4.

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 bomGenerator {
     // region Firework SDK
-    val fireworkSdkVersion = "6.4.0"
+    val fireworkSdkVersion = "6.3.4"
     includeDependency("com.firework", "sdk", fireworkSdkVersion)
 
     includeDependency("com.firework.external.imageloading", "glide", fireworkSdkVersion)
@@ -72,7 +72,7 @@ publishing {
 fun MavenPublication.configurePublication() {
     groupId = "com.firework"
     artifactId = "firework-bom"
-    version = "2023.08.11"
+    version = "2023.08.11-1"
 }
 
 fun MavenPublication.configurePom() {
