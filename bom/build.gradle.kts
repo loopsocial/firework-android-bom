9ec40e12adac235cbf7a7d4243ce9fecc2b01fad
Xiaosong Lin <105191928+xiaoslin91@users.noreply.github.com>
2024-08-27 11:54:05 +0800

    Upgrade to 6.14.0 (#26)

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 bomGenerator {
     // region Firework SDK
-    val fireworkSdkVersion = "6.13.1"
+    val fireworkSdkVersion = "6.14.0"
     includeDependency("com.firework", "sdk", fireworkSdkVersion)
 
     includeDependency("com.firework.external.imageloading", "glide", fireworkSdkVersion)
@@ -66,7 +66,7 @@ publishing {
 fun MavenPublication.configurePublication() {
     groupId = "com.firework"
     artifactId = "firework-bom"
-    version = "2024.08.08"
+    version = "2024.08.26"
 }
 
 fun MavenPublication.configurePom() {
