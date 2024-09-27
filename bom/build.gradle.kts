df40e05c0fb424fc54013b9a4ee8178f39a0a820
Eggsy <eggsy@fireworkhq.com>
2024-09-27 21:05:00 +0800

    Release version 2024.09.27

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 bomGenerator {
     // region Firework SDK
-    val fireworkSdkVersion = "6.14.1"
+    val fireworkSdkVersion = "6.14.2"
     includeDependency("com.firework", "sdk", fireworkSdkVersion)
 
     includeDependency("com.firework.external.imageloading", "glide", fireworkSdkVersion)
@@ -66,7 +66,7 @@ publishing {
 fun MavenPublication.configurePublication() {
     groupId = "com.firework"
     artifactId = "firework-bom"
-    version = "2024.09.12"
+    version = "2024.09.27"
 }
 
 fun MavenPublication.configurePom() {
