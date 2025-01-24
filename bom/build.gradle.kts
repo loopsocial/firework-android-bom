9fec3f00404d7529d8be887a79866d86da86618d
Xiaosong Lin <xiaosong@fireworkhq.com>
2025-01-24 09:12:53 +0800

    Upgrade native sdk vesrion to 6.16.7

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 bomGenerator {
     // region Firework SDK
-    val fireworkSdkVersion = "6.16.6"
+    val fireworkSdkVersion = "6.16.7"
     includeDependency("com.firework", "sdk", fireworkSdkVersion)
 
     includeDependency("com.firework.external.imageloading", "glide", fireworkSdkVersion)
@@ -66,7 +66,7 @@ publishing {
 fun MavenPublication.configurePublication() {
     groupId = "com.firework"
     artifactId = "firework-bom"
-    version = "2025.01.21"
+    version = "2025.01.24"
 }
 
 fun MavenPublication.configurePom() {
