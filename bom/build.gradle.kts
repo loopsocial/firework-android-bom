3f4378c53d326eb36488d6385622bc42645a6a18
Eggsy <eggsy@fireworkhq.com>
2025-03-26 14:52:14 +0800

    Update native sdk version to 6.18.0

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 bomGenerator {
     // region Firework SDK
-    val fireworkSdkVersion = "6.17.1"
+    val fireworkSdkVersion = "6.18.0"
     includeDependency("com.firework", "sdk", fireworkSdkVersion)
 
     includeDependency("com.firework.external.imageloading", "glide", fireworkSdkVersion)
@@ -66,7 +66,7 @@ publishing {
 fun MavenPublication.configurePublication() {
     groupId = "com.firework"
     artifactId = "firework-bom"
-    version = "2025.03.04"
+    version = "2025.03.26"
 }
 
 fun MavenPublication.configurePom() {
