99055c74b15c890092427f14a2bfbac06725779c
Xiaosong Lin <xiaosong@fireworkhq.com>
2025-07-07 16:30:22 +0800

    Upgrade to 6.22.2

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 bomGenerator {
     // region Firework SDK
-    val fireworkSdkVersion = "6.22.0"
+    val fireworkSdkVersion = "6.22.2"
     includeDependency("com.firework", "sdk", fireworkSdkVersion)
 
     includeDependency("com.firework.external.imageloading", "glide", fireworkSdkVersion)
