3bbbf32ff665a01cd3486ff8585120d86d436d28
Xiaosong Lin <xiaosong@fireworkhq.com>
2025-08-11 14:24:10 +0800

    Upgrade native sdk version to 6.23.1

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 bomGenerator {
     // region Firework SDK
-    val fireworkSdkVersion = "6.23.0"
+    val fireworkSdkVersion = "6.23.1"
     includeDependency("com.firework", "sdk", fireworkSdkVersion)
 
     includeDependency("com.firework.external.imageloading", "glide", fireworkSdkVersion)
